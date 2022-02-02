f0bc4b191b995835d675379f267de364260b5740
shaul stavi <shaulstavi@gmail.com>
2022-02-02 15:00:08 -0500

    userProfileDto

---
+++ My2Cents.API/My2Cents.DataInfrastructure/Models/UserProfileDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My2Cents.DataInfrastructure.Models
+{
+  internal class UserProfileDto
+  {
+    public int UserId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? SecondaryEmail { get; set; }
+    public string? MailingAddress { get; set; }
+    public int? Phone { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Employer { get; set; }
+    public string? WorkAddress { get; set; }
+    public int? WorkPhone { get; set; }
+
+  }
+}
