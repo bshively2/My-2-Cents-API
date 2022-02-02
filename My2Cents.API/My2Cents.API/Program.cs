750dbdf52f9bf629121bd5888b8927362221a82d
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-02-02 12:08:30 -0600

    passed the connection string to EfRepository

--- My2Cents.API/My2Cents.API/Program.cs
+++ My2Cents.API/My2Cents.API/Program.cs
@@ -1,6 +1,10 @@
+using My2Cents.DataInfrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 string connectionString = builder.Configuration.GetConnectionString("connectionString");
+
+IRepository repository = new EfRepository(connectionString);
 
 var app = builder.Build();
 
