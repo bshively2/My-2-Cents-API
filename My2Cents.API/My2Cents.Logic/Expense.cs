5c7b408960c25678da59696a41bf5665cdec0f46
Ben Herbert <50344850+ben22020@users.noreply.github.com>
2022-02-02 10:56:10 -0500

    added income and expense objects

---
+++ My2Cents.API/My2Cents.Logic/Expense.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My2Cents.Logic
+{
+    public class Expense
+    {
+            public string? name { get; set; }
+            public int? account { get; set; }
+            public string? itemName { get; set; }
+            public decimal? price { get; set; }
+            public string? detail { get; set; }
+            public string? date { get; set; }
+
+       
+        public Expense(string name, int account, string itemName, decimal price, string detail, string date)
+        {
+            
+            this.name = name;
+            this.account = account;
+            this.itemName = itemName;
+            this.price = price;
+            this.detail = detail;
+            this.date = date;
+        }
+    }
+}
---
+++ My2Cents.API/My2Cents.Logic/Income.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My2Cents.Logic
+{
+    internal class Income
+    {
+        public string? name { get; set; }
+        public int? account { get; set; }
+        public decimal? amount { get; set; }
+
+        public Income(string name, int account, decimal amount)
+        {
+            this.name = name;
+            this.account = account;
+            this.amount = amount;
+        }
+    }
+}
