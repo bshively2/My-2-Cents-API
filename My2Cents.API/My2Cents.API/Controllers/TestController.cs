56b52de2969134d6b95490241a8fa5e438ca27a4
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-02-02 13:24:49 -0600

    add test controller

---
+++ My2Cents.API/My2Cents.API/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My2Cents.API.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
