6d54cdda228869c342b394ccbbc8e6ea667cf3a2
mwaggoner4 <39037847+mwaggoner4@users.noreply.github.com>
2022-02-03 14:32:43 -0600

    updated the ScaffoldModel

--- My2Cents.API/My2Cents.DataInfrastructure/ScaffoldModel/My2CentsContext.cs
+++ My2Cents.API/My2Cents.DataInfrastructure/ScaffoldModel/My2CentsContext.cs
@@ -17,7 +17,9 @@ namespace My2Cents.DataInfrastructure
         }
 
         public virtual DbSet<Account> Accounts { get; set; } = null!;
+        public virtual DbSet<AccountType> AccountTypes { get; set; } = null!;
         public virtual DbSet<Transaction> Transactions { get; set; } = null!;
+        public virtual DbSet<UserLogin> UserLogins { get; set; } = null!;
         public virtual DbSet<UserProfile> UserProfiles { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -28,7 +30,7 @@ namespace My2Cents.DataInfrastructure
 
                 entity.Property(e => e.AccountId).HasColumnName("AccountID");
 
-                entity.Property(e => e.AccountType).HasMaxLength(40);
+                entity.Property(e => e.AccountTypeId).HasColumnName("AccountTypeID");
 
                 entity.Property(e => e.Interest).HasColumnType("decimal(18, 0)");
 
@@ -36,10 +38,28 @@ namespace My2Cents.DataInfrastructure
 
                 entity.Property(e => e.UserId).HasColumnName("UserID");
 
+                entity.HasOne(d => d.AccountType)
+                    .WithMany(p => p.Accounts)
+                    .HasForeignKey(d => d.AccountTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("ACC_FK_AccTypeID");
+
                 entity.HasOne(d => d.User)
                     .WithMany(p => p.Accounts)
                     .HasForeignKey(d => d.UserId)
-                    .HasConstraintName("FK__Account__UserID__70DDC3D8");
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("ACC_FK_UserID");
+            });
+
+            modelBuilder.Entity<AccountType>(entity =>
+            {
+                entity.ToTable("AccountType");
+
+                entity.Property(e => e.AccountTypeId).HasColumnName("AccountTypeID");
+
+                entity.Property(e => e.AccountType1)
+                    .HasMaxLength(40)
+                    .HasColumnName("AccountType");
             });
 
             modelBuilder.Entity<Transaction>(entity =>
@@ -52,28 +72,52 @@ namespace My2Cents.DataInfrastructure
 
                 entity.Property(e => e.Authorized).HasMaxLength(20);
 
-                entity.Property(e => e.TransactionDate).HasColumnType("datetime");
+                entity.Property(e => e.LineAmount).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.TransactionDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
 
                 entity.Property(e => e.TransactionName).HasMaxLength(100);
 
                 entity.HasOne(d => d.Account)
                     .WithMany(p => p.Transactions)
                     .HasForeignKey(d => d.AccountId)
-                    .HasConstraintName("FK__Transacti__Accou__73BA3083");
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Ts_FK_AccID");
+            });
+
+            modelBuilder.Entity<UserLogin>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__User_Log__1788CCAC16385041");
+
+                entity.ToTable("User_Login");
+
+                entity.HasIndex(e => e.UserName, "UQ__User_Log__C9F284560BC6EAFF")
+                    .IsUnique();
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.Email).HasMaxLength(255);
+
+                entity.Property(e => e.Password).HasMaxLength(255);
+
+                entity.Property(e => e.UserName).HasMaxLength(255);
             });
 
             modelBuilder.Entity<UserProfile>(entity =>
             {
                 entity.HasKey(e => e.UserId)
-                    .HasName("PK__User_Pro__1788CCACB4290265");
+                    .HasName("PK__User_Pro__1788CCAC4512CFFA");
 
                 entity.ToTable("User_Profile");
 
-                entity.Property(e => e.UserId).HasColumnName("UserID");
+                entity.Property(e => e.UserId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("UserID");
 
                 entity.Property(e => e.City).HasMaxLength(40);
-
-                entity.Property(e => e.Email).HasMaxLength(250);
 
                 entity.Property(e => e.Employer).HasMaxLength(150);
 
@@ -83,13 +127,21 @@ namespace My2Cents.DataInfrastructure
 
                 entity.Property(e => e.MailingAddress).HasMaxLength(250);
 
+                entity.Property(e => e.Phone).HasMaxLength(10);
+
                 entity.Property(e => e.SecondaryEmail).HasMaxLength(250);
 
-                entity.Property(e => e.State)
-                    .HasMaxLength(40)
-                    .HasColumnName("State_");
+                entity.Property(e => e.State).HasMaxLength(40);
 
                 entity.Property(e => e.WorkAddress).HasMaxLength(250);
+
+                entity.Property(e => e.WorkPhone).HasMaxLength(10);
+
+                entity.HasOne(d => d.User)
+                    .WithOne(p => p.UserProfile)
+                    .HasForeignKey<UserProfile>(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_UserID");
             });
 
             OnModelCreatingPartial(modelBuilder);
