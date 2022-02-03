cfdf0d2f9811616f876f80c1d81075e54384ffd5
Melinda Waggoner <melinda.a.waggoner@gmail.com>
2022-02-03 14:27:59 -0600

    updated the scaffold

--- My2Cents.API/My2Cents.DataInfrastructure/ScaffoldModel/UserProfile.cs
+++ My2Cents.API/My2Cents.DataInfrastructure/ScaffoldModel/UserProfile.cs
@@ -11,18 +11,18 @@ namespace My2Cents.DataInfrastructure
         }
 
         public int UserId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
         public string? SecondaryEmail { get; set; }
-        public string? MailingAddress { get; set; }
-        public double? Phone { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? Employer { get; set; }
-        public string? WorkAddress { get; set; }
-        public double? WorkPhone { get; set; }
+        public string MailingAddress { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string Employer { get; set; } = null!;
+        public string WorkAddress { get; set; } = null!;
+        public string WorkPhone { get; set; } = null!;
 
+        public virtual UserLogin User { get; set; } = null!;
         public virtual ICollection<Account> Accounts { get; set; }
     }
 }
